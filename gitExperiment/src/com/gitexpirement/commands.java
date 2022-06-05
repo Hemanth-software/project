058239d8ace9cc9db328057a95f7c0b416df66c2
Hemanth-software <84978130+Hemanth-software@users.noreply.github.com>
2022-06-05 14:42:12 -0400

    third commit

--- gitExperiment/src/com/gitexpirement/commands.java
+++ gitExperiment/src/com/gitexpirement/commands.java
@@ -3,7 +3,11 @@ package com.gitexpirement;
 public class commands {
 
 	public static void main(String[] args) {
-      System.out.println("east or west , I am the fucking best");
+      System.out.println("east or west , I am the fucking best,");
+      System.out.println("I want to be sucessful in my life");
+
+		
+	
 	}
 
 }
